78ca8de212fe20f0af38728c3b7a0d0088f4d51f
DriLLFreAK100 <blusky_8893@hotmail.com>
2020-10-08 00:43:23 +0800

    feat: include typography, color, button updates

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -17,3 +17,33 @@ export const Primary = Template.bind({});
 Primary.args = {
   text: 'Click Me!',
 } as ButtonProps;
+
+export const Subtle = Template.bind({});
+Subtle.args = {
+  text: 'Click Me!',
+  type: 'subtle',
+} as ButtonProps;
+
+export const Info = Template.bind({});
+Info.args = {
+  text: 'Click Me!',
+  type: 'info',
+} as ButtonProps;
+
+export const Success = Template.bind({});
+Success.args = {
+  text: 'Click Me!',
+  type: 'success',
+} as ButtonProps;
+
+export const Warning = Template.bind({});
+Warning.args = {
+  text: 'Click Me!',
+  type: 'warning',
+} as ButtonProps;
+
+export const Error = Template.bind({});
+Error.args = {
+  text: 'Click Me!',
+  type: 'error',
+} as ButtonProps;
--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,82 @@
-const path = require("path");
-const webpack = require("webpack");
+/* eslint-disable global-require */
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: "./src/index.ts",
-    mode: "development",
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx|ts|tsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                use: ["babel-loader", , "eslint-loader"],
-                options: { presets: ["@babel/env"] }
+  entry: './src/index.ts',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: { presets: ['@babel/env'] },
+          },
+          'eslint-loader',
+        ],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: {
+                localIdentName: '[local]-[hash:base64:5]',
+              },
             },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 1,
-                            modules: {
-                                localIdentName: '[path][name]__[local]--[hash:base64:5]'
-                            },
-                        }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            implementation: require("sass"),
-                        }
-                    }
-                ],
-                include: /\.module\.css$/
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
             },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            implementation: require("sass")
-                        }
-                    }
-                ],
-                exclude: /\.module\.css$/
+          },
+        ],
+        include: /\.module\.css$/,
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
             },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-            },
-        ]
-    },
-    resolve: { extensions: ["*", ".js", ".jsx"] },
-    output: {
-        path: path.resolve(__dirname, "dist/"),
-        publicPath: "/dist/",
-        filename: "bundle.js"
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "public/"),
-        port: 3000,
-        publicPath: "http://localhost:3000/dist/",
-        hot: true,
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-    ]
+          },
+        ],
+        exclude: /\.module\.css$/,
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public/'),
+    port: 3000,
+    publicPath: 'http://localhost:3000/dist/',
+    hot: true,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 };
