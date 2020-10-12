e632f8e02a7e7cf503e0737a7baf80de81e2947e
DriLLFreAK100 <blusky_8893@hotmail.com>
2020-10-13 01:10:42 +0800

    chore: update storybook theming

---
+++ .storybook/codefeeKitTheme.js
@@ -0,0 +1,28 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+
+  colorPrimary: '#00838f',
+  colorSecondary: '#ffc107',
+
+  // UI
+  appBg: 'white',
+  appContentBg: '#fafafa',
+  appBorderRadius: 4,
+
+  // Typography
+  fontBase: 'Roboto, sans-serif',
+
+  // Text colors
+  textColor: 'black',
+  textInverseColor: 'black',
+
+  // Toolbar default and active colors
+  barTextColor: 'white',
+  barSelectedColor: '#fff350',
+  barBg: '#00838f',
+
+  brandTitle: 'Codefee-Kit Storybook',
+  brandUrl: 'https://github.com/DriLLFreAK100/codefee-kit',
+});
