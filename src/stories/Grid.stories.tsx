47b0e32abe89f1600e172038e3e5300a3b1124c4
DriLLFreAK100 <blusky_8893@hotmail.com>
2020-10-23 23:29:39 +0800

    fix(grid): fix story control knob

--- src/stories/Grid.stories.tsx
+++ src/stories/Grid.stories.tsx
@@ -5,27 +5,37 @@ import CommonHelper from 'utils/CommonHelper';
 
 export default {
   title: 'Layout/Grid',
+  component: Grid,
 } as Meta;
 
+const Template: Story<IGrid> = (args) => <Grid {...args} />;
+
 const sampleData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-export const Sample: Story<IGrid> = () => {
-  return (
-    <Grid fullHeight>
-      {sampleData.map((s) => {
-        return (
-          <Grid
-            key={s}
-            xs={6}
-            lg={4}
-            xl={3}
-            style={{ background: CommonHelper.getRandomColor() }}
-            xAlign="center"
-            yAlign="center"
-          >
-            {s}
-          </Grid>
-        );
-      })}
-    </Grid>
-  );
-};
+
+export const Sample = Template.bind({});
+Sample.args = {
+  fullHeight: true,
+  children: () => {
+    return (
+      <>
+        {
+          sampleData.map((s) => {
+            return (
+              <Grid
+                key={s}
+                xs={6}
+                lg={4}
+                xl={3}
+                style={{ background: CommonHelper.getRandomColor() }}
+                xAlign="center"
+                yAlign="center"
+              >
+                {s}
+              </Grid>
+            );
+          })
+        }
+      </>
+    );
+  },
+} as IGrid;
