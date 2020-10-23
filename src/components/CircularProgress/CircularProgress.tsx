af8a890c2018e877d04488db0594aa34c0a3582b
DriLLFreAK100 <blusky_8893@hotmail.com>
2020-10-24 02:04:20 +0800

    feat(circular-progress): include determinate and indeterminate

--- src/components/CircularProgress/CircularProgress.tsx
+++ src/components/CircularProgress/CircularProgress.tsx
@@ -1,16 +1,58 @@
 import React, { FC } from 'react';
+import StringHelper from 'utils/StringHelper';
 import styles from './CircularProgress.module.scss';
 
 interface CircularProgressProps {
-
+  color?: 'primary' | 'secondary';
+  progress?: number;
+  type?: 'determinate' | 'indeterminate';
 }
 
-const CircularProgress: FC<CircularProgressProps> = () => {
+const radius = 20.2;
+const circumference = 2 * Math.PI * radius;
+
+const CircularProgress: FC<CircularProgressProps> = ({
+  color,
+  progress,
+  type,
+}: CircularProgressProps) => {
+  const determinateStyle = {
+    strokeDasharray: circumference,
+    strokeDashoffset: `${circumference - (((progress as number) / 100) * circumference)}`,
+  };
+
+  const circularProgressClassName = StringHelper.flatten(`
+    ${styles['circularProgress']} 
+    ${styles[`circularProgress--${type}`]}
+  `);
+
+  const circleClassName = StringHelper.flatten(`
+    ${styles[`circularProgress__svg--${color}`]} 
+    ${styles[`circularProgress__svg--${type}`]}
+  `);
+
   return (
-    <div className={styles['circularProgress']}>
-      Test
+    <div className={circularProgressClassName}>
+      <svg className={styles['circularProgress__svg']} viewBox="22 22 44 44">
+        <circle
+          className={circleClassName}
+          style={determinateStyle}
+          cx="44"
+          cy="44"
+          r={radius}
+          fill="none"
+          strokeWidth="3.6"
+        />
+      </svg>
     </div>
   );
+};
+
+CircularProgress.displayName = 'CircularProgress';
+CircularProgress.defaultProps = {
+  color: 'primary',
+  progress: 0,
+  type: 'indeterminate',
 };
 
 export default CircularProgress;
