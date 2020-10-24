f0aa0965232da3095e62fa1d045af9563fa6eee6
DriLLFreAK100 <blusky_8893@hotmail.com>
2020-10-25 01:30:08 +0800

    chore: webpack bundle configs

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+import Button from './Button';
+import CircularProgress from './CircularProgress';
+import Grid from './Grid';
+
+export {
+  Button,
+  CircularProgress,
+  Grid,
+};
+
+export * from './Button';
+export * from './CircularProgress';
+export * from './Grid';
---
+++ src/hooks/index.ts
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+import useWindowSize from './useWindowSize';
+
+export {
+  useWindowSize,
+};
