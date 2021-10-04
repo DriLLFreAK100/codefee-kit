d404c3fecf6bbf42bff4dc2c3a2431c3996e30c1
DriLLFreAK100 <blusky_8893@hotmail.com>
2021-10-05 01:13:04 +0800

    feat(separator): update to gray theme color

--- src/components/Separator/Separator.style.tsx
+++ src/components/Separator/Separator.style.tsx
@@ -1,11 +1,10 @@
 import styled from 'styled-components';
-import { rem } from 'utils';
+import { cvar, rem } from 'utils';
 
 export const LineSeparator = styled.hr`
-  background-image: linear-gradient(#222, #222);
+  background-image: linear-gradient(${cvar('--color-gray-7')}, ${cvar('--color-gray-7')});
   background-size: 100% ${rem(24)};
   border: 0;
-  color: #222;
   height: ${rem(4)};
   margin: ${rem(52)} auto;
   width: ${rem(100)};
@@ -14,7 +13,7 @@ export const LineSeparator = styled.hr`
 export const DotSeparator = styled.hr`
   border: 0;
   border-top: ${rem(6)} dotted;
-  color: #222;
+  color: ${cvar('--color-gray-7')};
   margin: ${rem(47)} auto;
   width: ${rem(48)};
 `;
