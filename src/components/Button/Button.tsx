63591414240262de09e3c95c1fb26f3041a1efaf
DriLLFreAK100 <blusky_8893@hotmail.com>
2021-10-05 00:18:30 +0800

    refactor(button): split styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,8 @@
-import styled from 'styled-components';
-import { cvar, rem } from 'utils/StyleHelper';
+import Typography from 'components/Typography';
 import React, {
   ReactNode, forwardRef, ButtonHTMLAttributes,
 } from 'react';
-import Typography from 'components/Typography';
+import * as S from './Button.style';
 
 export type ButtonType = 'primary' | 'subtle' | 'info' | 'success' | 'warning' | 'error';
 
@@ -14,151 +13,22 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   onClick?: () => void;
 }
 
-const StyledButton = styled.button<ButtonProps>`
-  border: none;
-  border-radius: ${cvar('--control-border-radius')};
-  box-sizing: border-box;
-  box-shadow: ${cvar('--control-shadow')};
-  cursor: pointer;
-  height: ${cvar('--control-height')};
-  min-width: ${rem(130)};
-  outline: none;
-  padding: ${rem(14)} ${rem(16)};
-  transition: background-color ${cvar('--transition-hover')} ease-in-out;
-
-  &:disabled {
-    cursor: not-allowed;
-    opacity: 0.6;
-    transition: none;
-  }
-`;
-
-const PrimaryButton = styled(StyledButton)`
-  background-color: ${cvar('--color-primary')};
-  color: ${cvar('--color-primary-on')};
-
-  :hover {
-    background:  ${cvar('--color-primary-light')};
-  }
-
-  :active {
-    background:  ${cvar('--color-primary-dark')};
-  }
-
-  :disabled {
-    background-color:  ${cvar('--color-primary')};
-  }
-`;
-
-const SubtleButton = styled(StyledButton)`
-  background-color: white;
-  border: ${rem(1)} solid  ${cvar('--color-primary')};
-  padding: ${rem(13)} ${rem(16)};
-  color: black;
-
-  :hover {
-    border: ${rem(1)} solid  ${cvar('--color-primary-light')};
-    background: ${cvar('--color-primary-light')};
-    color: ${cvar('--color-primary-on')};
-  }
-
-  :active {
-    border: ${rem(1)} solid ${cvar('--color-primary-dark')};
-    background: ${cvar('--color-primary-dark')};
-    color:  ${cvar('--color-primary-on')};
-  }
-
-  :disabled {
-    border: ${rem(1)} solid ${cvar('--color-primary')};
-    background-color: white;
-    color: black;
-  }
-`;
-
-const InfoButton = styled(StyledButton)`
-  background-color: ${cvar('--color-info')};
-  color: ${cvar('--color-info-on')};
-
-  :hover {
-    background: ${cvar('--color-info-light')};
-  }
-
-  :active {
-    background: ${cvar('--color-info-dark')};
-  }
-
-  :disabled {
-    background-color: ${cvar('--color-info')};
-  }
-`;
-
-const SuccessButton = styled(StyledButton)`
-  background-color: ${cvar('--color-success')};
-  color: ${cvar('--color-success-on')};
-
-  :hover {
-    background: ${cvar('--color-success-light')};
-  }
-
-  :active {
-    background: ${cvar('--color-success-dark')};
-  }
-
-  :disabled {
-    background-color: ${cvar('--color-success')};
-  }
-`;
-
-const WarningButton = styled(StyledButton)`
-  background-color: ${cvar('--color-warning')};
-  color: ${cvar('--color-warning-on')};
-
-  :hover {
-    background: ${cvar('--color-warning-light')};
-  } 
-
-  :active {
-    background: ${cvar('--color-warning-dark')};
-  } 
-  
-  :disabled {
-    background-color: ${cvar('--color-warning')};
-  }
-`;
-
-const ErrorButton = styled(StyledButton)`
-  background-color: ${cvar('--color-error')};
-  color: ${cvar('--color-error-on')};
-
-  :hover {
-    background: ${cvar('--color-error-light')};
-  } 
-
-  :active {
-    background: ${cvar('--color-error-dark')};
-  } 
-  
-  :disabled {
-    background-color: ${cvar('--color-error')};
-  }
-`;
-
 const getButtonComponent = (type: ButtonType) => {
   switch (type) {
     case 'primary':
-      return PrimaryButton;
+      return S.PrimaryButton;
     case 'subtle':
-      return SubtleButton;
+      return S.SubtleButton;
     case 'info':
-      return InfoButton;
+      return S.InfoButton;
     case 'success':
-      return SuccessButton;
+      return S.SuccessButton;
     case 'warning':
-      return WarningButton;
+      return S.WarningButton;
     case 'error':
-      return ErrorButton;
+      return S.ErrorButton;
     default:
-      return PrimaryButton;
+      return S.PrimaryButton;
   }
 };
 
@@ -177,7 +47,6 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
     return (
       <Component
         ref={ref}
-        variant={variant as ButtonType}
         onClick={onClick}
         {...passThrough}
       >
