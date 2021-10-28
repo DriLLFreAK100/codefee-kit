4f7fdc7ae26bcc4025442446a1ee4d74949a8577
DriLLFreAK100 <blusky_8893@hotmail.com>
2021-10-28 19:12:48 +0800

    fix: breaking change to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import AppContainer from '../src/components/AppContainer';
+import { AppContainer } from '../src/components/AppContainer';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
