234a904e380831c2ee64fe0361f820353994f082
DriLLFreAK100 <blusky_8893@hotmail.com>
2021-11-21 14:05:31 +0800

    feat(multiclickarea): hoc sample

--- src/stories/MultiClickArea.stories.tsx
+++ src/stories/MultiClickArea.stories.tsx
@@ -1,8 +1,10 @@
+import image from './assets/filter-coffee.jpg';
 import MultiClickArea, { MultiClickAreaProps } from 'components/MultiClickArea';
 import React from 'react';
 import styles from './assets/styles/MultiClickArea.module.scss';
-import { Meta, Story } from '@storybook/react/types-6-0';
+import withMultiClick from 'components/MultiClickArea/withMultiClick';
 import { action } from '@storybook/addon-actions';
+import { Meta, Story } from '@storybook/react/types-6-0';
 import { Typography } from 'components/Typography';
 /* eslint-disable no-alert */
 
@@ -37,3 +39,15 @@ TripleClick.args = {
   debounceTime: 500,
   onClick: action('triple click'),
 } as MultiClickAreaProps;
+
+export const WithMultiClickHoc = () => {
+  const cssClass = `${styles.multiClickArea} ${styles['multiClickArea--img']}`;
+
+  return withMultiClick(
+    <img className={cssClass} src={image} />,
+    {
+      onClick: action('double click'),
+      className: styles['multiClickArea--inlineBlock'],
+    },
+  );
+}
