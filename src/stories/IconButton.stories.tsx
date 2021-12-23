1b18bc032c0f5f3350ce2437bd5974050a73cf44
DriLLFreAK100 <blusky_8893@hotmail.com>
2021-12-23 22:30:55 +0800

    fix(icon-button): incorrect type

--- src/stories/IconButton.stories.tsx
+++ src/stories/IconButton.stories.tsx
@@ -1,6 +1,7 @@
 import IconButton, { IconButtonProps } from 'components/IconButton';
 import React from 'react';
 import { Meta, Story } from '@storybook/react/types-6-0';
+import { action } from '@storybook/addon-actions';
 import { Times } from 'components/Icons';
 import styles from './assets/styles/IconButton.module.scss';
 /* eslint-disable no-alert */
@@ -19,9 +20,11 @@ const Template: Story<IconButtonProps> = (args) => (
 export const Primary = Template.bind({});
 Primary.args = {
   variant: 'primary',
+  onClick: action('Clicked!'),
 } as IconButtonProps;
 
 export const Secondary = Template.bind({});
 Secondary.args = {
   variant: 'secondary',
+  onClick: action('Clicked!'),
 } as IconButtonProps;
