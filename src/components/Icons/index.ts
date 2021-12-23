4cd91d78cce471b4a42d052b35a54f0d76288c08
DriLLFreAK100 <blusky_8893@hotmail.com>
2021-12-23 22:42:19 +0800

    feat(icon): bars icon

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,4 +1,6 @@
 export { default as AngleDown } from './AngleDown';
+export { default as Bars } from './Bars';
 export { default as Coffee } from './Coffee';
+export { default as Icon } from './Icon';
 export { default as Star } from './Star';
 export { default as Times } from './Times';
