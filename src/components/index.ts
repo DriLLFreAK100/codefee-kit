e52b12a501c4b3661691e81eaef53243eceb0d04
DriLLFreAK100 <blusky_8893@hotmail.com>
2021-12-28 10:03:47 +0800

    chore: expose link component

--- src/components/index.ts
+++ src/components/index.ts
@@ -23,6 +23,7 @@ export * from './Icons';
 export { default as ImageViewer } from './ImageViewer';
 export * from './ImageViewer';
 
+export { default as Link } from './Link';
 export * from './Link';
 
 export { default as LoadArea } from './LoadArea';
