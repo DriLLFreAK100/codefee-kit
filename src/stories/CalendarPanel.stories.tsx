a388c13a7eefaa4a0f946b23b65f82f1f3da02bc
DriLLFreAK100 <blusky_8893@hotmail.com>
2022-02-20 23:07:45 +0800

    docs(calendar-panel): sample custom labels

--- src/stories/CalendarPanel.stories.tsx
+++ src/stories/CalendarPanel.stories.tsx
@@ -18,3 +18,13 @@ const Template: Story<CalendarPanelProps> = (args: CalendarPanelProps) => {
 
 export const Default = Template.bind({});
 Default.args = {} as CalendarPanelProps;
+
+export const CustomDayIndicatorLabels = Template.bind({});
+CustomDayIndicatorLabels.args = {
+  dayIndicatorLabels: ['日', '一', '二', '三', '四', '五', '六'],
+} as CalendarPanelProps;
+
+export const CustomMonthLabels = Template.bind({});
+CustomMonthLabels.args = {
+  monthLabels: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+} as CalendarPanelProps;
