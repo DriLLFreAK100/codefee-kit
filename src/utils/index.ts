b93f4b01138015d2af04be7bd819c2c5380fb0d8
drillfreak100 <blusky_8893@hotmail.com>
2022-11-14 23:53:15 +0800

    fix(theme-helper): override partial type

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,5 +4,5 @@ export * from './DateHelper';
 export * from './MathHelper';
 export * from './StringHelper';
 export * from './StyleHelper';
-export * from './ThemeHelper';
+export * from './ThemeHelper/ThemeHelper';
 export * from './TimeHelper';
